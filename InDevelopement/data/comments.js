032ca836a7d22397f831f60e060f02f902920401
malhar-paghdal <95156459+malhar-paghdal@users.noreply.github.com>
2021-11-30 22:20:46 +0530

    Added Comment File

---
+++ InDevelopement/data/comments.js
@@ -0,0 +1,33 @@
+const { MongoClient, ObjectID, ReadPreferenceMode } = require('mongodb');
+const mongoCollections = require("../config/mongoCollections");
+const comments = mongoCollections.comments;
+
+// Note: 
+// Pass only userID which user has inserted comment
+// And content of Comment
+// Comment ID & Comment date will generate by createComment
+async function createComment(userId, Content) {
+    if (typeof userId !== 'string')
+        throw 'UserId should be string';
+    if (typeof Content !== 'string')
+        throw 'Content should be string';
+
+    const commentsCollection = await comments();
+    var CommentId = (new ObjectID).toString();
+    let CommentDate = new Date().toUTCString();
+
+    let obj = {
+        CommentId: CommentId,
+        userId: userId,
+        Content: Content,
+        Date: CommentDate
+    }
+    const insertInfo = await commentsCollection.insertOne(obj);
+    if (insertInfo.insertedCount === 0) {
+        throw "Error: Comment not inserted"
+    }
+    return obj;
+}
+module.exports = {
+    createComment
+};
