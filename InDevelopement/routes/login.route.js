cee282a0ce8c43df5e790ee50d4060ef8dcd1ed7
Hiren <>
2021-12-06 21:14:37 +0530

    add session

--- InDevelopement/routes/login.route.js
+++ InDevelopement/routes/login.route.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
+const { ObjectId } = require("bson");
 const users = data.users;
 
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-router.get("/login", async (req, res) => {
+router.get("/login", async(req, res) => {
     res.render(`login/login`);
 })
 
-router.post("/login", async (req, res) => {
+router.post("/login", async(req, res) => {
     try {
         let email = req.body.username;
         let accountPassword = req.body.password;
         let checkUser = await users.checkUser(email, accountPassword);
+        let userId = ObjectId(checkUser._id).toString();
+        req.session.userauth = { name: checkUser.firstName + ' ' + checkUser.lastName, email: checkUser.email, user_id: userId };
         res.redirect('/');
     } catch (e) {
-        console.log(e, 'test');
         res.render(`login/login`, { err: e });
     }
 })
 
 
-router.get("/registration", async (req, res) => {
+router.get("/registration", async(req, res) => {
     res.render(`login/registration`);
 })
+
+router.get('/logout', async(req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
 
 
 module.exports = router;
