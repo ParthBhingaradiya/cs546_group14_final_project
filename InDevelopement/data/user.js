5ec49655e6bb4bc3e76e817ac623eaab0147c181
malhar-paghdal <95156459+malhar-paghdal@users.noreply.github.com>
2021-12-07 23:53:23 +0530

    Added PreviousPurchaseItem function logic

--- InDevelopement/data/user.js
+++ InDevelopement/data/user.js
@@ -221,6 +221,57 @@ async function showWishlistItem(userid){
     const user = await getSingleUser(userid);
     return user.wishlist;
 }
+
+//Call this function while user purchase some items, we are adding item id to prevPurchase array
+async function addPurchaseItem(userid, itemid){
+    if (typeof userid != "string") {
+        throw "Error: was not given the right ID for the user"
+    }
+    if (typeof itemid != "string") {
+        throw "Error: was not given the right ID for the item"
+    }
+    const user1 = await getSingleUser(userid);
+    const newlist = user1.prevPurchase;
+    newlist.push(itemid);
+    let parseId;
+    try {
+        parseId = ObjectId(userid);
+    } catch (e) {
+        "Error: item id could not be converted into object id."
+    }
+    const doc = {
+        firstName: user1.firstName,
+        lastName: user1.lastName,
+        email: user.email,
+        address: user1.address,
+        city: user1.city,
+        pincode: user1.pincode,
+        state: user1.state,
+        accountPassword: user1.accountPassword,
+        age: user1.age,
+        avgRating: user1.avgRating,
+        prevPurchase:newlist,
+        prevSold: user1.prevSold,
+        commentSeller: user1.commentSeller,
+        cart: user1.cart,
+        wishlist: user1.wishlist
+    }
+    const userCollection = await user();
+
+    const updatedInfo = await userCollection.updateOne({ _id: parseId }, { $set: doc });
+    if (updatedInfo.modifiedCount == 0) {
+        throw "Error: Could not update anything."
+    }
+    return "Added";
+}
+//It will return array of item ids of previous puschase item
+async function showPreviousPurchaseItem(userid){
+    if (typeof userid != "string") {
+        throw "Error: was not given the right ID for the user"
+    }
+    const user = await getSingleUser(userid);
+    return user.prevPurchase;
+}
 module.exports = {
     createUser,
     checkUser,
@@ -228,5 +279,7 @@ module.exports = {
     addToCartitem,
     addToWishlistitem,
     showCartItem,
-    showWishlistItem
+    showWishlistItem,
+    addPurchaseItem,
+    showPreviousPurchaseItem
 };
